2c1fc2a030ca7e2fdb7a055a06f604085f2ef817
Steven F <stevfabre@gmail.com>
2021-07-23 12:38:05 +0200

    Mise en place du moteur de recherche V1

--- scripts/List.js
+++ scripts/List.js
@@ -3,10 +3,31 @@ class List {
     this.all = [];
     this.filtered = new Set();
     this.selected = new Set();
+    this.simplified = new Set();
   }
 
   add(item) {
     this.all.push(item);
+  }
+
+  createSimplifiedList() {
+    let simplifiedRecipe;
+    this.all.forEach((recipe) => {
+      simplifiedRecipe = { id: recipe.id, words: new Set() };
+      recipe.ingredients.forEach((ingredient) => {
+        createSimplifiedWordList(ingredients.normalizeInput(ingredient.ingredient));
+      });
+      createSimplifiedWordList(ingredients.normalizeInput(recipe.name));
+      createSimplifiedWordList(ingredients.normalizeInput(recipe.description));
+      simplifiedRecipe.words = Array.from(simplifiedRecipe.words);
+      this.simplified.add(simplifiedRecipe);
+    });
+
+    function createSimplifiedWordList(arrayOfWords) {
+      arrayOfWords.forEach((word) => {
+        if (word.length > 2) simplifiedRecipe.words.add(word);
+      });
+    }
   }
 
   display() {
@@ -92,6 +113,33 @@ class List {
     this.display();
   }
 
+  search() {
+    function debounce(callback, delay) {
+      let timer = null;
+      return function () {
+        let context = this;
+        let args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+          callback.apply(context, args);
+        }, delay);
+      };
+    }
+
+    document.getElementById("main__search").addEventListener(
+      "keyup",
+      debounce(function (e) {
+        if (e.target.value.length > 2) {
+          list.matchingRecipes(e.target.value);
+          list.display();
+        } else {
+          list.filtered = list.all;
+          list.display();
+        }
+      }, 600)
+    );
+  }
+
   hideList() {
     document.querySelectorAll(".dropdown").forEach((e) => e.classList.remove("dropdown__active"));
     document.querySelectorAll(".secondary__list").forEach((e) => e.classList.add("hide"));
@@ -105,6 +153,17 @@ class List {
     appliances.renderItem("appliances");
     ustensils.renderItem("ustensils");
     this.display();
+    this.search();
+    this.createSimplifiedList();
+  }
+
+  matchingRecipes(input) {
+    this.filtered = new Set();
+    this.simplified.forEach((recipe) => {
+      if (ingredients.normalizeInput(input).every((element) => recipe.words.find((item) => item.includes(element)))) {
+        this.filtered.add(this.all.find((meal) => meal.id == recipe.id));
+      }
+    });
   }
 
   resetFilters() {
